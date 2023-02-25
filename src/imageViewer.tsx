d17cafe9cbbe7219827574b3536a5ccba15d4f29
liulirui <liulirui@blacklake.cn>
2023-02-25 11:45:09 +0800

    fix: fix some animated actions

--- src/imageViewer.tsx
+++ src/imageViewer.tsx
@@ -20,6 +20,7 @@ import {
   Image,
   TouchableOpacity,
   ImageResizeMode,
+  StatusBar,
 } from 'react-native';
 import Animated, {
   runOnJS,
@@ -34,6 +35,7 @@ import {
   Gesture,
   GestureStateChangeEvent,
   PanGestureHandlerEventPayload,
+  GestureHandlerRootView,
 } from 'react-native-gesture-handler';
 import { useStateRef } from 'react-hooks-extension';
 
@@ -57,6 +59,9 @@ export type ImageViewerRef = {
 const IMAGE_SPACE = 20;
 
 const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
   absolute: {
     position: 'absolute',
   },
@@ -127,7 +132,6 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
       const changeHeight =
         (((currentOriginalImageSize?.height || 0) - screenDimensions.height) *
           (imageScaleValue === 1 ? imageYValue : 1)) /
-        imageScaleValue /
         screenDimensions.height;
       const manualWidth = Math.min(
         screenDimensions.width,
@@ -136,7 +140,7 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
       );
       const manualHeight = Math.min(currentHeight, currentHeight + changeHeight);
       const manualTop =
-        (screenDimensions.height - currentHeight) / 2 + imageYValue / imageScaleValue;
+        ((screenDimensions.height - currentHeight) / 2 + imageYValue) / imageScaleValue;
       const manualLeft = imageXValue / imageScaleValue;
       return {
         width: manualWidth + ((activeLayoutValue?.width || 0) - manualWidth) * closeRateValue,
@@ -147,7 +151,7 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
             translateX:
               manualLeft -
               (screenDimensions.width + IMAGE_SPACE) * activeIndexValue +
-              ((activeLayoutValue?.pageX || 0) - manualLeft) * closeRateValue +
+              (((activeLayoutValue?.pageX || 0) - manualLeft) * closeRateValue) / imageScaleValue +
               (imagePosition -
                 1 +
                 Math.floor(activeIndexValue / 3) * 3 +
@@ -159,7 +163,9 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
                 (screenDimensions.width + IMAGE_SPACE),
           },
           {
-            translateY: manualTop + ((activeLayoutValue?.pageY || 0) - manualTop) * closeRateValue,
+            translateY:
+              manualTop +
+              (((activeLayoutValue?.pageY || 0) - manualTop) * closeRateValue) / imageScaleValue,
           },
         ],
         display: ((relativeActiveIndex + 1) % 3 !== imagePosition &&
@@ -289,6 +295,12 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
             animatedRate.value = withTiming(1, undefined, (finished) => {
               finished && runOnJS(setAnimatedOver)(true);
             });
+            closeRate.value = withTiming(1, undefined, (finished) => {
+              if (finished) {
+                runOnJS(onCloseFinish)(!!imageSize.value[data[activeIndex.value].key]);
+                animatedRate.value = 0;
+              }
+            });
           }, 0);
         },
       );
@@ -296,60 +308,54 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
     [activeLayout, animatedRate],
   );
   const onClose = useWorkletCallback(() => {
+    imageScale.value = withTiming(1);
     runOnJS(onCloseMeasure)(activeIndex.value);
-    imageScale.value = 1;
     savedImageScale.value = 1;
-    closeRate.value = withTiming(1, undefined, (finished) => {
-      if (finished) {
-        runOnJS(onCloseFinish)(!!imageSize.value[data[activeIndex.value].key]);
-        animatedRate.value = 0;
-      }
-    });
+    savedImageX.value = 0;
+    savedImageY.value = 0;
   }, [data]);
 
-  const onEndScalePanInnerX = useWorkletCallback(() => {
-    const currentWidthRange = (screenDimensions.width * (imageScale.value - 1)) / 2;
-    const currentImageX = Math.min(currentWidthRange, Math.max(-currentWidthRange, imageX.value));
-    if (currentImageX !== imageX.value) {
-      imageX.value = withTiming(currentImageX);
-    }
-    savedImageX.value = currentImageX;
-  }, []);
-  const onEndScalePanInnerY = useWorkletCallback(() => {
-    const currentImageSize = imageSize.value[data[activeIndex.value].key];
-    const currentImageHeight =
-      (screenDimensions.width * (currentImageSize.height || 1)) / (currentImageSize.width || 1);
-    const initY = (currentImageHeight - screenDimensions.height) / 2;
-    const extraHeight = screenDimensions.height - currentImageHeight * imageScale.value + initY;
-    const currentImageY = Math.min(
-      extraHeight,
-      Math.max(Math.min(initY, extraHeight), imageY.value),
-    );
-    if (currentImageY !== imageY.value) {
-      imageY.value = withTiming(currentImageY);
-    }
-    savedImageY.value = currentImageY;
-  }, []);
   const onEndScalePan = useWorkletCallback(
     (event?: GestureStateChangeEvent<PanGestureHandlerEventPayload>) => {
-      if (event) {
-        imageX.value = withDecay(
-          {
-            velocity: event.velocityX,
-            clamp: [imageX.value, imageX.value + (event.velocityX > 0 ? 30 : -30)],
-          },
-          onEndScalePanInnerX,
+      const currentWidthRange = (screenDimensions.width * (imageScale.value - 1)) / 2;
+      const currentImageX = Math.min(currentWidthRange, Math.max(-currentWidthRange, imageX.value));
+      if (currentImageX !== imageX.value) {
+        imageX.value = withTiming(currentImageX);
+        savedImageX.value = currentImageX;
+      } else if (event?.velocityX) {
+        const targetImageX = Math.min(
+          currentWidthRange,
+          Math.max(-currentWidthRange, imageX.value + (event.velocityX > 0 ? 30 : -30)),
         );
-        imageY.value = withDecay(
-          {
-            velocity: event.velocityY,
-            clamp: [imageY.value, imageY.value + (event.velocityY > 0 ? 30 : -30)],
-          },
-          onEndScalePanInnerY,
+        savedImageX.value = targetImageX;
+        imageX.value = withDecay({
+          velocity: event.velocityX,
+          clamp: [imageX.value, targetImageX],
+        });
+      }
+      const currentImageSize = imageSize.value[data[activeIndex.value].key];
+      const currentImageHeight =
+        (screenDimensions.width * (currentImageSize.height || 1)) / (currentImageSize.width || 1);
+      const currentHeightRange = Math.abs(
+        (currentImageHeight * imageScale.value - screenDimensions.height) / 2,
+      );
+      const currentImageY = Math.min(
+        currentHeightRange,
+        Math.max(-currentHeightRange, imageY.value),
+      );
+      if (currentImageY !== imageY.value) {
+        imageY.value = withTiming(currentImageY);
+        savedImageY.value = currentImageY;
+      } else if (event?.velocityY) {
+        const targetImageY = Math.min(
+          currentHeightRange,
+          Math.max(-currentHeightRange, imageY.value + (event.velocityY > 0 ? 30 : -30)),
         );
-      } else {
-        onEndScalePanInnerX();
-        onEndScalePanInnerY();
+        savedImageY.value = targetImageY;
+        imageY.value = withDecay({
+          velocity: event.velocityY,
+          clamp: [imageY.value, targetImageY],
+        });
       }
     },
     [screenDimensions.width, data],
@@ -359,12 +365,12 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
   const imageDragGestureY = useMemo(
     () =>
       Gesture.Pan()
-        .activeOffsetY(1)
+        .activeOffsetY(5)
         .onStart(() => {
           if (imageScale.value === 1) {
             runOnJS(hideOriginalImage)();
+            dragLastTime.value = Date.now().valueOf();
           }
-          dragLastTime.value = Date.now().valueOf();
         })
         .onUpdate((event) => {
           if (imageScale.value !== 1) {
@@ -414,7 +420,7 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
   const imageDragGestureX = useMemo(
     () =>
       Gesture.Pan()
-        .activeOffsetX([-1, 1])
+        .activeOffsetX([-5, 5])
         .onStart(() => {
           dragLastTime.value = Date.now().valueOf();
         })
@@ -454,6 +460,7 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
           } else {
             imageX.value = withTiming(0);
           }
+          savedImageX.value = 0;
         }),
     [
       dragLastTime,
@@ -469,7 +476,15 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
       _onChange,
     ],
   );
-  const imageTapGesture = useMemo(
+  const resetScale = useWorkletCallback(() => {
+    imageScale.value = withTiming(1);
+    imageX.value = withTiming(0);
+    imageY.value = withTiming(0);
+    savedImageScale.value = 1;
+    savedImageX.value = 0;
+    savedImageY.value = 0;
+  }, []);
+  const imageSingleTapGesture = useMemo(
     () =>
       Gesture.Tap().onStart(() => {
         if (imageScale.value === 1) {
@@ -479,43 +494,53 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
       }),
     [onClose, hideOriginalImage, imageScale],
   );
+  const imageDoubleTapGesture = useMemo(
+    () =>
+      Gesture.Tap()
+        .numberOfTaps(2)
+        .onStart((event) => {
+          if (imageScale.value !== 1) {
+            resetScale();
+          } else {
+            imageScale.value = withTiming(3);
+            savedImageScale.value = 3;
+            const currentX = (screenDimensions.width / 2 - event.x) * 3;
+            imageX.value = withTiming(currentX);
+            savedImageX.value = currentX;
+            const currentY = (screenDimensions.height / 2 - event.y) * 3;
+            imageY.value = withTiming(currentY);
+            savedImageY.value = currentY;
+          }
+        }),
+    [resetScale, imageScale],
+  );
+  const imageTapGesture = useMemo(
+    () => Gesture.Exclusive(imageDoubleTapGesture, imageSingleTapGesture),
+    [imageDoubleTapGesture, imageSingleTapGesture],
+  );
   const imagePinchGesture = useMemo(
     () =>
       Gesture.Pinch()
         .onUpdate((event) => {
           imageScale.value = savedImageScale.value * event.scale;
+          const currentX = (screenDimensions.width / 2 - event.focalX) * imageScale.value;
+          imageX.value = currentX;
+          savedImageX.value = currentX;
+          const currentY = (screenDimensions.height / 2 - event.focalY) * imageScale.value;
+          imageY.value = currentY;
+          savedImageY.value = currentY;
         })
         .onEnd(() => {
-          imageScale.value = withTiming(Math.max(1, imageScale.value), undefined, () => {
-            savedImageScale.value = imageScale.value;
-            if (imageScale.value === 1) {
-              imageX.value = 0;
-              imageY.value = 0;
-              savedImageX.value = 0;
-              savedImageY.value = 0;
-            } else {
-              onEndScalePan();
-            }
-          });
+          const currentScale = Math.max(1, imageScale.value);
+          if (currentScale === 1) {
+            resetScale();
+          } else {
+            imageScale.value = withTiming(currentScale);
+            savedImageScale.value = currentScale;
+          }
         }),
-    [imageScale, savedImageScale, imageX, imageY, savedImageY, savedImageX, onEndScalePan],
+    [imageScale, savedImageScale, imageX, imageY, savedImageY, savedImageX, resetScale],
   );
-  const imagePinchPanGesture = useMemo(
-    () =>
-      Gesture.Pan()
-        .enableTrackpadTwoFingerGesture(true)
-        .minPointers(2)
-        .onUpdate((event) => {
-          imageX.value = savedImageX.value + event.translationX;
-          imageY.value = savedImageY.value + event.translationY;
-        })
-        .onEnd(() => {
-          savedImageX.value = imageX.value;
-          savedImageY.value = imageY.value;
-        }),
-    [imageX, imageY, savedImageX, savedImageY],
-  );
-  const imagePinchComposedGesture = Gesture.Simultaneous(imagePinchGesture, imagePinchPanGesture);
   const imageLongPressGesture = useMemo(
     () =>
       Gesture.LongPress().onStart(() => {
@@ -531,14 +556,14 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
         imageDragGestureY,
         imageDragGestureX,
         imageTapGesture,
-        imagePinchComposedGesture,
+        imagePinchGesture,
         imageLongPressGesture,
       ),
     [
       imageDragGestureY,
       imageDragGestureX,
       imageTapGesture,
-      imagePinchComposedGesture,
+      imagePinchGesture,
       imageLongPressGesture,
     ],
   );
@@ -591,63 +616,69 @@ const ImageViewer = forwardRef<ImageViewerRef, ImageViewerProps>((props, ref) =>
 
   return (
     <Modal visible={!!activeSource} animationType="fade" transparent onRequestClose={onClose}>
-      {activeSource ? (
-        <>
-          <GestureDetector gesture={imageGesture}>
-            <Animated.View style={[styles.animatedContainer, imageContainerStyle]}>
-              {Array.from(new Array(3)).map((_, index) => {
-                if (!animatedOver && !activeIndexState) {
-                  return null;
-                }
-                const relativeActiveIndex = ((activeIndexState! % 3) + 3) % 3;
-                const currentIndex =
-                  Math.floor(activeIndexState! / 3) * 3 +
-                  (relativeActiveIndex > index && activeIndexState! > 0
-                    ? 3
-                    : relativeActiveIndex <= index && activeIndexState! < 0
-                    ? -3
-                    : 0) +
-                  index -
-                  1;
-                if (currentIndex < 0 || currentIndex >= data.length) {
-                  return null;
-                }
-                const currentData = data[currentIndex];
-                return (
-                  <Animated.Image
-                    key={`image-viewer-${currentIndex}`}
-                    resizeMode={imageResizeMode}
-                    source={
-                      typeof currentData.source === 'object'
-                        ? { ...currentData.source }
-                        : currentData.source
-                    }
-                    onLoad={({ nativeEvent: { source } }) => {
-                      imageSize.value = { ...imageSize.value, [currentData.key]: source };
-                      setLoading(false);
-                      setFinishInit(true);
-                    }}
-                    style={[styles.absolute, imageStyleList[index]]}
+      <StatusBar backgroundColor="#000" barStyle="light-content" />
+      <GestureHandlerRootView style={styles.full}>
+        {activeSource ? (
+          <>
+            <GestureDetector gesture={imageGesture}>
+              <Animated.View style={[styles.animatedContainer, imageContainerStyle]}>
+                {Array.from(new Array(3)).map((_, index) => {
+                  if (!animatedOver && !activeIndexState) {
+                    return null;
+                  }
+                  const relativeActiveIndex = ((activeIndexState! % 3) + 3) % 3;
+                  const currentIndex =
+                    Math.floor(activeIndexState! / 3) * 3 +
+                    (relativeActiveIndex > index && activeIndexState! > 0
+                      ? 3
+                      : relativeActiveIndex <= index && activeIndexState! < 0
+                      ? -3
+                      : 0) +
+                    index -
+                    1;
+                  if (currentIndex < 0 || currentIndex >= data.length) {
+                    return null;
+                  }
+                  const currentData = data[currentIndex];
+                  return (
+                    <Animated.Image
+                      key={`image-viewer-${currentIndex}`}
+                      resizeMode={imageResizeMode}
+                      source={
+                        typeof currentData.source === 'object'
+                          ? { ...currentData.source }
+                          : currentData.source
+                      }
+                      onLoad={({ nativeEvent: { source } }) => {
+                        imageSize.value = { ...imageSize.value, [currentData.key]: source };
+                        setLoading(false);
+                        setFinishInit(true);
+                      }}
+                      style={[styles.absolute, imageStyleList[index]]}
+                    />
+                  );
+                })}
+                {loading ? (
+                  <ActivityIndicator
+                    style={[StyleSheet.absoluteFill, styles.loading]}
+                    color="#fff"
                   />
-                );
-              })}
-              {loading ? (
-                <ActivityIndicator style={[StyleSheet.absoluteFill, styles.loading]} color="#fff" />
-              ) : null}
-            </Animated.View>
-          </GestureDetector>
-          {!animatedOver || !finishInit ? (
-            <View style={[StyleSheet.absoluteFill, styles.animatedContainer]}>
-              <Animated.Image
-                source={typeof activeSource === 'object' ? { ...activeSource } : activeSource}
-                resizeMode="contain"
-                style={[styles.absolute, originalImageStyle]}
-              />
-            </View>
-          ) : null}
-        </>
-      ) : null}
-      {renderCustomComponent?.({ item: data[activeIndexState!], index: activeIndexState! })}
+                ) : null}
+              </Animated.View>
+            </GestureDetector>
+            {!animatedOver || !finishInit ? (
+              <View style={[StyleSheet.absoluteFill, styles.animatedContainer]}>
+                <Animated.Image
+                  source={typeof activeSource === 'object' ? { ...activeSource } : activeSource}
+                  resizeMode="contain"
+                  style={[styles.absolute, originalImageStyle]}
+                />
+              </View>
+            ) : null}
+          </>
+        ) : null}
+        {renderCustomComponent?.({ item: data[activeIndexState!], index: activeIndexState! })}
+      </GestureHandlerRootView>
     </Modal>
   );
 });
